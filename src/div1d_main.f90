b9f61f4ccfd5c55dcecb8b3742c6c89ff6cd0583
Gijs Derks <derks@rs2n.rijnh.nl>
2021-10-13 10:09:06 +0200

    commit before correction of reaction rates

--- src/div1d_main.f90
+++ src/div1d_main.f90
@@ -281,8 +281,8 @@ subroutine write_solution( time )
    write( 10, * ) 'dyn_qparX   = ', dyn_qparX(itime)
    write( 10, * ) 'dyn_red_frc = ', dyn_red_frc(itime)
    write( 10, '(A195)' ) '    X [m]        N [/m^3]       V [m/s]         T [eV]        Nn [/m^3]      Gamma_n    Gamma_mom [Pa]      q_parallel    neutral_flux     Source_n       Source_v       Source_Q     source_neut  '
-   write( 10, '(13(1PE15.6))' ) ( x(i), density(i), velocity(i), temperature(i), neutral(i), &
-   &                                   Gamma_n(i)*B_field(i), Gamma_mom(i)*B_field(i),q_parallel(i)*B_field(i),neutral_flux(i)*B_field(i), & ! multiplied by B_field because the code calculates with normalized values
+   write( 10, '(13(1PE15.6))' ) ( x(i), density(i), velocity(i), temperature(i), neutral(i), &                 !!!! the neutral flux should not be multiplied with the B_field !!!!    
+   &                                   Gamma_n(i)*B_field(i),Gamma_mom(i)*B_field(i),q_parallel(i)*B_field(i),neutral_flux(i)*B_field(i), & ! multiplied by B_field because the code uses normalized values
    &                                   Source_n(i), Source_v(i), Source_Q(i), source_neutral(i), i=1,Nx )
    
    return
