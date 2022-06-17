96c5804a32f5ffb4b17a8d6923929a98dda282fc
Gijs Derks <derks@rs2n.rijnh.nl>
2022-06-17 16:01:08 +0200

    defined fortran workflow, next is the matlab workflow and scope of variables

--- src/div1d_main.f90
+++ src/div1d_main.f90
@@ -31,141 +31,31 @@ program div1d
    type (VODE_OPTS) :: options
 
    ! the following is used to format for Matlab integration
-   real(wp), allocatable :: float__div1d(:)
-   integer,  allocatable :: int_div1d(:)
-   logical               :: log_phys
-
+   real(wp), allocatable :: float_div1d(20) = 0
+   integer,  allocatable :: int_div1d(20) = 0
+   logical               :: log_phys = .true.
+   integer, :: call_from_matlab = 0
 
    ! time the program execution time exluding external system calls (ifortran, gfortran) incl external system calls (solaris f90)
    real(wp) :: T1,T2
    call cpu_time(T1)
 
-   ! read non-default input parameters
-   call read_numerics_parameters(input_error_numerics)
-   call read_physics_parameters(input_error_physics)
-   input_error = input_error_numerics + 10*input_error_physics
-   if( input_error .ne. 0 ) call error_report(input_error, restart_error, time_step_error)
-
-   ! cast inputs to those needed by wrapper
-        float_div1d(1) = gamma
-        float_div1d(2) = L
-        float_div1d(3) = sintheta
-        float_div1d(4) = mass
-        float_div1d(5) = Gamma_X
-        float_div1d(6) = q_parX
-        float_div1d(7) = flux_expansion
-        float_div1d(8) = initial_n
-        float_div1d(9) = initial_a
-        float_div1d(10) = initial_v
-        float_div1d(11) = initial_T
-        float_div1d(12) = density_ramp_rate
-        float_div1d(13) = ...
-
-        int_div1d(1) = num_impurites
-        int_div1d(2:6) = impurity_Z
-        int_div1d(7) = elm_start_time
-        int_div1d(8) = elm_ramp_time
-        int_div1d(9) = elm_time_between
-        int_div1d(10)= switch_elm_heat_flux
-        int_div1d(11)= switch_elm_density
-        int_div1d(12)= switch_elm_series
-        int_div1d(13)= radial_loss_gaussian
-        int_div1d(14) = Nx
-        int_div1d(15) = ntime
-        int_div1d(16) = nout
-        int_div1d(17) = method
-        int_div1d(18) = .... 
-
-        log_phys = case_AMJUEL
-
-
-   ! initialize the grid
-   call initialize_grid
-
-   ! set the initial value of the solution vector
-   if( simple_sol ) then 
-      call init_simple_sol
-   else
-      call initial_values
-   endif
+   ! Initialization from FORTRAN will look for .txt and .dat files
+   call initialize_div1d(call_from_matlab, float_div1d, int_div1d, log_phys)
 
    start_time = 0.0
-   restart_error = 0
-   if( restart ) call read_restart_file( restart_error )
-   if( restart .and. restart_error .ne. 0 ) call error_report(input_error, restart_error, time_step_error)
-   
-   ! initialize the gas_puff
-   call initialize_gas_puff(Nx)
 
-   ! write the inital solution to file
-   ! calculate the fluxes
-   call calculate_fluxes( Nx, start_time,  density, velocity, temperature, neutral, Gamma_n, Gamma_mom, q_parallel, neutral_flux )
-   ! calculate the sources
-   call calculate_sources( Nx, start_time,  density, velocity, temperature, neutral, q_parallel, &
-                          Source_n, Source_v, Source_Q, source_neutral )
+   ! only fortran writes a text file
    open( UNIT=10, FILE='div1d_output.txt' )
    call write_header
    call write_solution( start_time )
   
-   ! setting the absolute error tolerance for dvode_f90 or dlsode
-   ! note that the solution vector y is normalized so the absolute error tolarance in all elements of y can be made equal
-   allocate( abstol_vector(4*Nx) )
-   abstol_vector(1:4*Nx) = abstol
-   ! abstol_vector(1:Nx) = initial_n * abstol
-   ! abstol_vector(Nx+1:2*Nx) = 1.0
-   ! abstol_vector(2*Nx+1:3*Nx) = initial_T * abstol
-   ! abstol_vector(3*Nx+1:4*Nx) = initial_n * abstol
-   
-   ! enforce nonnegative densities and energies by lower bounds in dvode_f90
-   allocate( bounded_components(Nx), lower_bounds(Nx), upper_bounds(Nx) )
-   ! plasma density / only energy for now
-   do ix = 1, Nx
-      bounded_components(ix) = ix + 2*Nx
-   enddo
-   ! energy
-   ! bounded_components(  Nx+1:2*Nx) = bounded_components(1:Nx) + 2 * Nx
-   ! neutral density
-   ! bounded_components(2*Nx+1:3*Nx) = bounded_components(1:Nx) + 3 * Nx
-   ! set lower bounds to zero
-   lower_bounds = 0.0d+0
-   ! set artifically high upper bounds
-   upper_bounds = 1.0d+50
-
-   ! setting the options fo dvode_f90
-   ! rough estimate of maximum number of nonzeros in Jacobian
-   nzswag_input = 48 * Nx
-   if( method .gt. 0 ) then 
-      options = set_opts(RELERR=reltol, ABSERR_VECTOR=abstol_vector, &
-                         CONSTRAINED=bounded_components, CLOWER=lower_bounds, CUPPER=upper_bounds,  &
-                         METHOD_FLAG=method, MXSTEP=max_step, NZSWAG=nzswag_input, MA28_ELBOW_ROOM=200, MA28_RPS=.TRUE.)
-      ! ! call set_jacobian_sparsity_structure
-      ! call set_diagonal_jacobian
-      ! call USERSETS_IAJA(IAUSER,NIAUSER,JAUSER,NJAUSER)
-
-   elseif( method .lt. 0 ) then
-      ! allocate arrays needed by dlsode
-      ! RWORK :WORK   Real work array of length at least:
-      !        20 + 16*NEQ                    for MF = 10,
-      !        22 +  9*NEQ + NEQ**2           for MF = 21 or 22,
-      !        22 + 10*NEQ + (2*ML + MU)*NEQ  for MF = 24 or 25.
-      ! LRW   :IN     Declared length of RWORK (in user's DIMENSION statement).
-      ! IWORK :WORK   Integer work array of length at least:
-      !        20        for MF = 10,
-      !        20 + NEQ  for MF = 21, 22, 24, or 25.
-      ! If MF = 24 or 25, input in IWORK(1),IWORK(2) the lower and upper Jacobian half-bandwidths ML,MU.
-      ! LIW   :IN     Declared length of IWORK (in user's DIMENSION statement).
-      ml = 3*Nx
-      mu = 3*Nx
-      lrw = 22 + (20+2*ml+mu)*4*Nx
-      liw = 20 + 4*Nx
-      allocate( rwork(lrw), iwork(liw) )
-      iwork(1) = ml
-      iwork(2) = mu
-      iwork(6) = max_step ! maximum number of internal iterations
-   endif
-        
-       
-   
+ 
+   istep = 1
+   do ibigstep = 1, nbigsteps
+        istate =1
+     
+   ! run nout time step_t in a function that can also be called by matlab
    call run_div1d(y, ydot, &
                 density, velocity, temperature, neutral,&
                 Gamma_n, Gamma_mom, pressure, q_parallel,&
@@ -175,76 +65,11 @@ program div1d
                 options, bounded_components, lower_bounds,&
                 upper_bounds, abstol_vector,&
                 float_div1d, int_div1d, log_phys)
-                  
-   istate = 1
-   do istep=1, ntime
-      write(*,*) 'start integration step nr.', istep
-      end_time = start_time+delta_t
-
-     if( method .gt. 0 ) then
-         ! we use dvode_f90 for the integration
-         ! CALL DVODE_F90(F,NEQ,Y,T,TOUT,ITASK,ISTATE,OPTIONS,J_FCN=JAC,G_FCN=GEX)
-         itask = 1 ! for normal computation in dvode_f90 till end_time
-   !      call dvode_f90( right_hand_side, 4*Nx, y, start_time, end_time, itask, istate, options )
-
-        call run_div1d(y, ydot, &
-                density, velocity, temperature, neutral,&
-                Gamma_n, Gamma_mom, pressure, q_parallel,&
-                Source_n, Source_v, Source_Q, source_neutral,&
-                x, xcb, delta_x, delta_xcb, B_field, B_field_cb,&
-                e_charge,, c, K_B, amu, me, pi,&
-                options, bounded_components, lower_bounds,&
-                upper_bounds, abstol_vector,&
-                float_div1d, int_div1d, log_phys)
-
-
-      elseif( method .lt. 0 ) then
-   !      ! we use dlsode.f for the integration
-         itask = 1 ! for normal computation in dvode_f90 till end_time
-         itol = 2 ! the absolute error tolerance is specified in an array
-         iopt = 1 ! optional inputs (upper and lower half bandwidths of the Jacobian)
-         ! CALL DLSODE(F,               NEQ, Y,          T,     TOUT, ITOL,   RTOL,          ATOL, ITASK, ISTATE, IOPT, RWORK, LRW, IWORK, LIW, JAC, MF)
-         call dlsode( right_hand_side, 4*Nx, y, start_time, end_time, itol, reltol, abstol_vector, itask, istate, iopt, rwork, lrw, iwork, liw, jac, abs(method) )
-      else
-         call rk4( right_hand_side, 4*Nx, y, start_time, end_time )
-      endif
-   !   ! make sure that the solution respects the minimum density and temperature
-   !   call y2nvt( Nx, y, density, velocity, temperature, neutral )
-   !   do ix = 1, Nx
-   !      if( temperature(ix) .lt. minimum_temperature) temperature(ix) = minimum_temperature
-   !      ! if( density(ix)     .lt. minimum_density)     density(ix) =     minimum_density
-   !      ! if( neutral(ix)     .lt. minimum_density)     neutral(ix) =     minimum_density
-   !   enddo
-    !  call nvt2y( Nx, density, velocity, temperature, neutral, y )
-   !   time_step_error = istate
-   !   if( istate .ne. 2 .and. method .gt. 0 ) then
-   !      attempt = 0
-   !      do while( istate .ne. 2 .and. attempt .le. max_attempts )
-   !         ! first try calling dvode again with istate = 1
-   !         attempt = attempt + 1
-   !         write(*,*) 'trying to continue integration. attempt nr.', attempt
-   !         istate = 1
-   !         call dvode_f90( right_hand_side, 4*Nx, y, start_time, end_time, itask, istate, options )
-   !      enddo
-   !      if( istate .ne. 2 ) then 
-   !         call error_report(input_error, restart_error, time_step_error)
-   !      else
-   !         write(*,*) 'success on ', attempt, 'th attempt. continuing'
-   !      endif
-   !   endif
-      if( mod( istep, nout ) .eq. 0 ) then
-         ! call y2nvt( Nx, y, density, velocity, temperature, neutral )
-         ! calculate the fluxes
-         call calculate_fluxes( Nx, start_time, density, velocity, temperature, neutral, Gamma_n, Gamma_mom, q_parallel, neutral_flux )
-   !      ! calculate the sources
-         call calculate_sources( Nx, start_time, density, velocity, temperature, neutral, q_parallel, Source_n, Source_v, Source_Q, source_neutral )
-         call write_solution( end_time )
-      endif
-      start_time = end_time
-      ! reset istate to 1 every so often
-      if( modulo( istep, istate_mod ) .eq. 0 ) istate = 1
-   enddo
-
+   ! FORTRAN Writes the solution   
+   call write_solution( end_time )
+   end do
+      
+   ! loop is over
    call cpu_time(T2)
 
 
--- src/div1d_solve.f90
+++ src/div1d_solve.f90
@@ -12,9 +12,13 @@ module div1d_solve
       use dvode_f90_m
       use experiments
         
-  contains
+        implicit none
 
-        subroutine initialize_div1d( y, ydot, &                                 ! plasma params
+  contains
+        
+
+
+         subroutine initialize_div1d( y, ydot, &                                 ! plasma params
                              density, velocity, temperature, neutral, &         ! plasma params 
                              Gamma_n, Gamma_mom, pressure, q_parallel, &        ! plasma params
                              Source_n, Source_v, Source_Q, source_neutral, &    ! plasma params 
@@ -22,22 +26,35 @@ module div1d_solve
                              e_charge, c, K_B, amu, me, pi,&                    ! constants
                              options, bounded_components, lower_bounds, &       ! solver options (DVODE only)
                              upper_bounds, abstol_vector, &                     ! solver options (DVODE only)
-                             floatindiv1d, intindiv1d, strinphys, loginphys)    ! phys+num params(INPUTS)
+                             floatindiv1d, intindiv1d, strinphys, loginphys,&   ! phys+num params(INPUTS)
+                             call_from_matlab)
                   !bind(c,name="initialize_div1d_")
             ! Returns initial-plasma values, grid data, constants, and solver options
-            
+            implicit none
+            integer, INTENT(IN) :: call_from_matlab
+            integer :: restart_error
+            if call_from_matlab .eq. 0
+            ! read non-default inputs from .txt and .dat files
+            call read_numerics_parameters(input_error_numerics)
+            call read_physics_parameters(input_error_physics)
+            else if call_from_matlab .eq. 1
+            ! read non-default settings from floatindiv1d and intindiv1d etc.
+            call read_numerics_par_mat(floatindiv1d, intindiv1d, strinphys, loginphys)
+            call read_physics_par_mat(floatindiv1d, intindiv1d, strinphys, loginphys)              
+            end if
         
             call initialize_grid
-            call initialize_gas_puff(Nx)
-            call read_restart_file( restart_error )
-            call initialize_solver_settings( solver_setup_error )
-            
-                
+            call initialize_values
 
+            restart_error = 0.0
+            if( restart )  call read_restart_file( restart_error )
+            if restart .and. restart_error .ne. 0 ) call error_report( input_error, restart_error, time_step_error)
+            call initialize_gas_puff(Nx)
+            call initialize_dvode_settings( solver_setup_error )
         end subroutine initialize_div1d
 
 
-        subroutine run_div1d(y, ydot, &                                         ! plasma params
+        subroutine run_div1d(y, &                                               ! plasma params
                              density, velocity, temperature, neutral, &         ! plasma params 
                              Gamma_n, Gamma_mom, pressure, q_parallel, &        ! plasma params
                              Source_n, Source_v, Source_Q, source_neutral, &    ! plasma params 
@@ -46,7 +63,16 @@ module div1d_solve
                              options, bounded_components, lower_bounds, &       ! solver options (DVODE only)
                              upper_bounds, abstol_vector, &                     ! solver options (DVODE only)
                              float_div1d, int_div1d, log_phys)                  ! phys+num params                              !bind(c,name="run_div1d_")
-       
+        implicit none
+        integer, parameter, private :: wp = KIND(1.0D0)
+        real( wp ),INTENT(IN) :: float_div1d(10)
+        integer,   INTENT(IN) :: int_div1d(10)
+        logical,   INTENT(IN) :: log_phys
+
+        real( wp ), :: Nx = float_div1d(1)
+        real( wp ), ::
+
+
         istate = 1
         do istep = 1, ntime
         end_time = start_time + delta_t
@@ -77,5 +103,34 @@ module div1d_solve
         ! routine below needs to be build?
         !call dy2dnvt( Nx, dy ,d_density,d_velocity,d_temperature, d_neutral)
         !end subroutine run_div1d_derivative
+
+        subroutine initialize_dvode_settings( options, reltol, abstol, lower_bounds, upper_bounds, Nx )
+        ! setting options for dvode_f90
+        implicit none
+        
+        ! setting absolute errror tolerance for dvode_f90 
+        ! (note that y is normalized resulting in equal tolerances)
+        allocate( abstol_vector(4*Nx) )
+        abstol_vector(1:4*Nx) = abstol
+        
+        ! enforce nonnegative densities and energies by lower bounds in dvode_f90
+        allocate ( bounded_components(Nx), lower_bounds(Nx), upper_bounds(Nx) )
+        do ix = 1,Nx
+                bounded_components( ix) = ix + 2*Nx
+        enddo
+
+        ! rough estimate of maximum number of nonzeros in Jacobian 
+        nzswag_input = 48 * Nx
+        ! build the options structure
+        options = set_opts(RELERR=reltol, ABSERR_VECTOR=abstol_vector, &
+                                CONSTRAINED=bounded_components, CLOWER=lower_bounds, CUPPER=upper_bounds, &
+                                METHOD_FLAG=method, MXSTEP=max_step, NZSWAG=nzswag_input, MA28_ELBOW_ROOM=200, &
+                                MA28_RPS=.TRUE. )
+        ! call set_jacobian_sparsity_structure
+        ! call set_diagnoal_jacobian
+        ! call USERSETS_IAJA(IAUSER,NIAUSER,JAUSER,NJAUSER)
+
+        end subroutine initialize_dvode_settings
+
 
 end module div1d_solve
